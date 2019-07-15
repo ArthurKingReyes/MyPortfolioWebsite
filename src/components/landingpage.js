3811411513008c551daee63c592bfe4dba59161f
Arthur Reyes <arthurreyes90@gmail.com>
2019-07-15 11:22:39 -0700

    added gradient background and avatar image

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
+import avatar from './images/avatar.png';
 
 class Landing extends Component{
     render(){
         return(
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                        src={avatar}
+                        alt="avatar"
+                        className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Software Developer</h1>
+                        <hr/>
+                        <p>HTML | CSS | JavaScript | React | Java | C/C++</p>
+                        </div>
+                    
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
