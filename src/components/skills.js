792208da44b11251b88ef264bb2ec34ea9f7c5ad
Arthur Reyes <arthurreyes90@gmail.com>
2019-07-16 18:46:28 -0700

    fixed progress bar unevennessby creting a grid with 2 cells within the cell. Next change will be to just make it as 2 cells originally so they wont be nested.

--- src/components/skills.js
+++ src/components/skills.js
@@ -6,9 +6,22 @@ class Skills extends Component{
         return(
             <Grid>
                 <Cell col={12}>
+                    <Grid>
+                        <Cell col={2}>
+                        <div style={{display: 'flex'}}>{this.props.skill}</div>
+                        </Cell>
+
+                        <Cell col={10}>
+                        <ProgressBar style={{margin:'auto', width:'80%'}} progress={this.props.progress} />
+                        </Cell>
+                    </Grid>
+                    
+                    
+                    
+                    {/*}
                     <div style={{display: 'flex'}}>{this.props.skill}
                     <ProgressBar style={{margin:'auto', width:'80%'}} progress={this.props.progress} />                  
-                    </div>
+                    </div> */}
                 </Cell>
             </Grid>
         )
