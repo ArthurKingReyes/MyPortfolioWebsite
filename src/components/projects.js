b70dbd9fcf9ccb6a9a3e6c30b9021177d58c3a76
Arthur Reyes <arthurreyes90@gmail.com>
2019-07-28 10:33:09 -0700

    Updated project cards with images of the actual projects. Added resized images for cards. Placed card title in card description.

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,19 +8,25 @@ class Projects extends Component{
     }
     
     toggleCategories(){
+        var wendlers = require('./images/wendlerssc.png');
+        var portfoliosite = require('./images/portfoliosite.png');
+        var pixabay = require('./images/pixabay.png');
+        
         if(this.state.activeTab === 0){
             return(
                 <div className="projects-grid">
 
                 {/** -- React Projects  -- **/}
-                <Card shadow="{5}" style={{minWidth: '450', margin:'auto'}}>
-                    <CardTitle style={{color: '#fff', height: '176px', background: 
-                    'url(https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png) center / cover'}}>Portfolio Website (This Site)</CardTitle>
-                    {/* change url to a screenshot of the website */}
+                <Card shadow="{5}" style={{minWidth: '540px', margin:'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '334px', background: 
+                    "url(" + portfoliosite + ")"}}></CardTitle>
+                    
                     <CardText> 
+                        <h6 style={{color: 'black'}}>Portfolio (This site)</h6>
                         My personal portfolio website exhibiting my projects as well as information about me.
                         Uses React-MDL for material ui.
                     </CardText>
+                    
                     <CardActions border>
                         <a href="https://github.com/ArthurKingReyes/MyPortfolioWebsite" rel="noopener noreferrer" target="_blank">
                             <Button colored>GitHub</Button>
@@ -28,13 +34,15 @@ class Projects extends Component{
                     </CardActions>
                 </Card>
 
-                <Card shadow="{5}" style={{minWidth: '450', margin:'auto'}}>
-                    <CardTitle style={{color: '#fff', height: '176px', background: 
-                    'url(https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png) center / cover'}}>Pixabay Image Finder</CardTitle>
-                    {/* change url to a screenshot of the website */}
+                <Card shadow="{5}" style={{minWidth: '540px', margin:'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '334px', background: 
+                    "url(" + pixabay + ")"}}></CardTitle>
+                    
                     <CardText> 
+                    <h6 style={{color: 'black'}}>Pixabay Image Finder</h6>
                     Uses the Pixabay REST API and Axios, a Promise based HTTP client, to retrieve images from the Pixabay website based on the search keyword the user inputs.
                     </CardText>
+                    
                     <CardActions border>
                         <a href="https://github.com/ArthurKingReyes/Pixabay-Image-Finder" rel="noopener noreferrer" target="_blank">
                             <Button colored>GitHub</Button>
@@ -54,13 +62,15 @@ class Projects extends Component{
                 <div className="projects-grid">
 
                 {/** -- JavaScript  Projects-- **/}
-                <Card shadow="{5}" style={{minWidth: '450', margin:'auto'}}>
-                    <CardTitle style={{color: 'black', height: '176px', background: 
-                    'url(https://i2.wp.com/www.nuimedia.com/wp-content/uploads/2017/01/javascript-logo.png?ssl=1) center / cover'}}>
-                       
-                    </CardTitle>
-                    {/* change url to a screenshot of the website */}
-                    <CardText><h5 style={{color: 'black'}}>Wendler's 5/3/1 Assistant</h5>A calculator based on the Wendler's 531 strength training program. </CardText>
+                <Card shadow="{5}" style={{minWidth: '450px', margin:'auto'}}>
+                    <CardTitle style={{color: 'white', height: '176px', background: 
+                    "url(" + wendlers + ")" }}></CardTitle>
+                    
+                    <CardText>
+                    <h6 style={{color: 'black'}}>Wendler's 5/3/1 Calculator</h6>    
+                    A calculator based on the Wendler's 531 strength training program. 
+                    </CardText>
+                    
                     <CardActions border>
                         <a href="https://laughing-pasteur-894d80.netlify.com/" rel="noopener noreferrer" target="_blank">
                             <Button colored>Demo</Button>
@@ -76,13 +86,16 @@ class Projects extends Component{
                 <div className="projects-grid">
 
                 {/** -- Java Projects  -- **/}
-                <Card shadow="{5}" style={{minWidth: '450', margin:'auto'}}>
+                <Card shadow="{5}" style={{minWidth: '250px', margin:'auto'}}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 
                     'url(https://static.techspot.com/images2/news/bigimage/2016/05/2016-05-31-image.jpg) center / cover'}}>
-                        Prime Number Generator
                     </CardTitle>
-                    {/* change url to a screenshot of the website */}
-                    <CardText>A multithreaded prime number generator.</CardText>
+                    
+                    <CardText>
+                    <h6 style={{color: 'black'}}>Prime Number Generator</h6>
+                    A multithreaded prime number generator written in Java.
+                    </CardText>
+                    
                     <CardActions border>
                         <a href="https://github.com/ArthurKingReyes/PrimeGenJava" rel="noopener noreferrer" target="_blank">
                             <Button colored>GitHub</Button>
